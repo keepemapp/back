3a26782c334f899a9b00b746cf6536159d9610f0
Marti <marti@8vi.cat>
2022-02-01 14:39:19 +0100

    Add cache header to asset file

--- kpm/assets/entrypoints/fastapi/v1/assets.py
+++ kpm/assets/entrypoints/fastapi/v1/assets.py
@@ -306,7 +306,9 @@ async def get_asset_file(
         # TODO when we go with encrypted files, media_type will be different
         try:
             return FileResponse(
-                file_repo.get(a["file_location"]), media_type=a["file_type"]
+                file_repo.get(a["file_location"]),
+                media_type=a["file_type"],
+                headers={'Cache-Control': 'private, max-age=2592000, immutable'}
             )
         except RuntimeError as e:
             raise HTTPException(detail=e.msg)
