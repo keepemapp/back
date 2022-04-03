70becaa3fcc08980b10d2ea47614d25a6c6f5194
martsec <marti@vcll.info>
2022-04-03 11:21:10 +0200

    fix some test setup

--- tests/assets/adapters/test_views.py
+++ tests/assets/adapters/test_views.py
@@ -41,18 +41,18 @@ class TestMemoryAssetViews:
         assert r.get("file_name") == asset.file.name
         assert r.get("id") == asset.id.id
 
-    def test_find_by_id(self, asset, bus_with_asset, views):
+    def test_find_by_id(self, active_asset, bus_with_asset, views):
         non_existing = views.find_by_id(
             "does not exist", bus=bus_with_asset
         )
         assert not non_existing
 
-        should_exist = views.find_by_id(asset.id.id, bus=bus_with_asset)
-        assert should_exist["id"] == asset.id.id
+        should_exist = views.find_by_id(active_asset.id.id, bus=bus_with_asset)
+        assert should_exist["id"] == active_asset.id.id
 
-    def test_find_id_and_owner(self, asset, bus_with_asset, views):
-        id = asset.id.id
-        owner = asset.owners_id[0].id
+    def test_find_id_and_owner(self, active_asset, bus_with_asset, views):
+        id = active_asset.id.id
+        owner = active_asset.owners_id[0].id
 
         id_non_existing = views.find_by_id_and_owner(
             "does not exist", owner, bus=bus_with_asset
@@ -67,7 +67,7 @@ class TestMemoryAssetViews:
         should_exist = views.find_by_id_and_owner(
             id, owner, bus=bus_with_asset
         )
-        assert should_exist["id"] == asset.id.id
+        assert should_exist["id"] == active_asset.id.id
 
     def test_asset_type_filter(self, active_asset, bus_with_asset, views):
         ftype = active_asset.file.type
@@ -84,20 +84,20 @@ class TestMemoryAssetViews:
         )
         assert len(different_type) == 0
 
-    def test_order(self, asset, bus_with_asset, valid_asset, views):
+    def test_order(self, active_asset, bus_with_asset, valid_asset, views):
         valid_asset["id"] = AssetId("secondAsset")
-        valid_asset["title"] = "zzzzzzzz"
+        valid_asset["title"] = "zzzzzz"
         second_asset = Asset(**valid_asset)
         add_asset_to_bus(second_asset, bus_with_asset)
 
         asc = views.all_assets(order_by="title", bus=bus_with_asset)
-        for a, b in zip(asc, [asset, second_asset]):
+        for a, b in zip(asc, [active_asset, second_asset]):
             assert a.get("title") == b.title
 
         desc = views.all_assets(
             order_by="title", order="desc", bus=bus_with_asset
         )
-        for a, b in zip(desc, [second_asset, asset]):
+        for a, b in zip(desc, [second_asset, active_asset]):
             assert a.get("title") == b.title
 
         with pytest.raises(AttributeError):
